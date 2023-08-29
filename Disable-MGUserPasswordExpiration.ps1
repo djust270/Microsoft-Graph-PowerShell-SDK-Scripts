09bff426ffecf21d6642f42234ff5f53a04a3236
David Just <57944742+djust270@users.noreply.github.com>
2023-08-29 16:50:39 -0400

    Update Disable-MGUserPasswordExpiration.ps1
    fixed typo

--- Disable-MGUserPasswordExpiration.ps1
+++ Disable-MGUserPasswordExpiration.ps1
@@ -6,9 +6,8 @@ param (
 [mailaddress]$UPN
 )
 $UPN = $UPN.Address
-Get-MgUser -UserId $UPN | select -ExpandProperty ID
 try {
-Invoke-MgGraphRequest -Method PATCH -Uri /v1.0/users/$userid -Body (@{"PasswordPolicies" = "DisablePasswordExpiration"}) -ErrorAction Stop
+Invoke-MgGraphRequest -Method PATCH -Uri /v1.0/users/$UPN -Body (@{"PasswordPolicies" = "DisablePasswordExpiration"}) -ErrorAction Stop
 "Sucessfully disabled password expiration for {0}" -f $UPN
 }
 Catch {
